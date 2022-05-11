2acffcc1255007d62e87900c7bc45d0150c41b8d
Frank <frankhagan890@gmail.com>
2022-05-11 09:01:03 -0400

    hi

--- NEWEST mENU.py
+++ NEWEST mENU.py
@@ -4,15 +4,18 @@ import runpy
 os.system("clear")
 
 
-main_menu = ["[a] main", "[b] run any script", "[c] realmenu.py", "[q] quit"]
+# add items to menu
+main_menu = ["[a] main", "[b] run any script", "[c] realmenu.py", "[d] second page", "[q] quit"]
+second_menu = ["[a] min2sec", "[b] mathfun", "[c]  math", "[q] quit"]
+
 
 loop = True
-
+#loop for selecting items
 while loop:
     choice = main_menu[TerminalMenu(main_menu, title="jame's menu").show()]
 
     if choice == "[a] main":
-        print (choice)
+        print(choice)
 
     elif choice == "[b] run any script":
         userchoice = input("What script would you like to run? ")
@@ -21,5 +24,11 @@ while loop:
     elif choice == "[c] realmenu.py":
         runpy.run_path("realmenu.py")
 
+    elif choice == "[d] second page":
+        choice2 = second_menu[TerminalMenu(second_menu, title="frank's menu").show()]
+
+    if choice2 == "[a] min2sec":
+            runpy.run_path("min2sec.py")
+        
     elif choice == "[q] quit":
         loop = False
